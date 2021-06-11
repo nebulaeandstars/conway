aa00ee16cf1397589640ed9765bdf0f490df305f
Laura Powles <laura@laurapowles.com>
2021-06-11 17:47:43 +1000

    stopped compensating for bad external DPI settings

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use glutin_window::GlutinWindow;
+use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventLoop, EventSettings, Events};
 use piston::input::ButtonState;
@@ -17,21 +17,28 @@ use crate::grid::Grid;
 fn main() {
     // default settings
     const OPENGL: OpenGL = OpenGL::V3_2;
-    const GRID_X: usize = 75;
-    const GRID_Y: usize = 50;
-    const TILE_SIZE: usize = 4;
+    const GRID_X: usize = 200;
+    const GRID_Y: usize = 100;
+    const TILE_SIZE: usize = 6;
     let colors = GameColors::new(
         [0.2, 0.2, 0.2, 1.0], // background
         [0.8, 0.8, 0.8, 1.0], // alive
         [0.2, 0.2, 0.2, 1.0], // dead
     );
 
-    let mut window: GlutinWindow = WindowSettings::new("snake", [600, 400])
-        .graphics_api(OPENGL)
-        .exit_on_esc(true)
-        .samples(2)
-        .build()
-        .unwrap();
+    let mut window: Window = WindowSettings::new(
+        "snake",
+        [
+            (TILE_SIZE * (GRID_X - 1)) as u32,
+            (TILE_SIZE * (GRID_Y - 1)) as u32,
+        ],
+    )
+    .graphics_api(OPENGL)
+    .exit_on_esc(true)
+    .resizable(false)
+    .samples(0)
+    .build()
+    .unwrap();
 
     let grid = Grid::random(GRID_X, GRID_Y);
     let mut game = Game::new(GlGraphics::new(OPENGL), grid, colors, TILE_SIZE);
